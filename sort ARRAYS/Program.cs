72e07f650e2195e181c54ce6a919d8f7f732324a
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-10-19 12:47:05 +0530

    initial commit

---
+++ sort ARRAYS/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+namespace sortArray
+{
+    class array
+    {
+        static void Main(string[] args)
+        {
+            int[] mynumber = { 121, 20, 13 };
+            string[] fruits = { "mango", "apple", "mosambi" };
+            mynumber[0] = 121;
+            mynumber[1] = 20;
+            mynumber[2] = 13;
+            Array.Sort(mynumber);
+            //
+            //Console.WriteLine(mynumber);
+             foreach (int number in mynumber)
+            {
+            Console.WriteLine(number);
+             }
+            Array.Sort(fruits);
+            foreach (string fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+    }
+}
